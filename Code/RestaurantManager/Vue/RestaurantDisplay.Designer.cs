6cb61d3dca5ae5db88b9b84f9bfd409ac3581678
OlivierDebray <olivier.debray@viacesi.fr>
2018-12-12 18:54:30 +0100

    Update RestaurantDisplay.Designer.cs

--- Code/RestaurantManager/Vue/RestaurantDisplay.Designer.cs
+++ Code/RestaurantManager/Vue/RestaurantDisplay.Designer.cs
@@ -32,8 +32,6 @@
             this.btn_Start = new System.Windows.Forms.Button();
             this.txtBox_Inspection = new System.Windows.Forms.TextBox();
             this.btn_Config = new System.Windows.Forms.Button();
-            this.lbl_HeureFixe = new System.Windows.Forms.Label();
-            this.lbl_HeureDyn = new System.Windows.Forms.Label();
             this.txtBox_Console = new System.Windows.Forms.TextBox();
             this.btn_Pause = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -46,13 +44,13 @@
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(919, 413);
+            this.panel1.Size = new System.Drawing.Size(981, 444);
             this.panel1.TabIndex = 0;
             // 
             // btn_Start
             // 
             this.btn_Start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btn_Start.Location = new System.Drawing.Point(12, 418);
+            this.btn_Start.Location = new System.Drawing.Point(12, 449);
             this.btn_Start.Name = "btn_Start";
             this.btn_Start.Size = new System.Drawing.Size(400, 50);
             this.btn_Start.TabIndex = 1;
@@ -63,7 +61,7 @@
             // txtBox_Inspection
             // 
             this.txtBox_Inspection.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtBox_Inspection.Location = new System.Drawing.Point(924, 12);
+            this.txtBox_Inspection.Location = new System.Drawing.Point(986, 12);
             this.txtBox_Inspection.Multiline = true;
             this.txtBox_Inspection.Name = "txtBox_Inspection";
             this.txtBox_Inspection.ReadOnly = true;
@@ -74,7 +72,7 @@
             // btn_Config
             // 
             this.btn_Config.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_Config.Location = new System.Drawing.Point(824, 418);
+            this.btn_Config.Location = new System.Drawing.Point(886, 449);
             this.btn_Config.Name = "btn_Config";
             this.btn_Config.Size = new System.Drawing.Size(389, 50);
             this.btn_Config.TabIndex = 3;
@@ -82,45 +80,25 @@
             this.btn_Config.UseVisualStyleBackColor = true;
             this.btn_Config.Click += new System.EventHandler(this.btn_Config_Click);
             // 
-            // lbl_HeureFixe
-            // 
-            this.lbl_HeureFixe.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.lbl_HeureFixe.AutoSize = true;
-            this.lbl_HeureFixe.Location = new System.Drawing.Point(924, 395);
-            this.lbl_HeureFixe.Name = "lbl_HeureFixe";
-            this.lbl_HeureFixe.Size = new System.Drawing.Size(55, 17);
-            this.lbl_HeureFixe.TabIndex = 4;
-            this.lbl_HeureFixe.Text = "Heure :";
-            // 
-            // lbl_HeureDyn
-            // 
-            this.lbl_HeureDyn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.lbl_HeureDyn.AutoSize = true;
-            this.lbl_HeureDyn.Location = new System.Drawing.Point(985, 395);
-            this.lbl_HeureDyn.Name = "lbl_HeureDyn";
-            this.lbl_HeureDyn.Size = new System.Drawing.Size(44, 17);
-            this.lbl_HeureDyn.TabIndex = 5;
-            this.lbl_HeureDyn.Text = "10:00";
-            // 
             // txtBox_Console
             // 
             this.txtBox_Console.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtBox_Console.Location = new System.Drawing.Point(924, 106);
+            this.txtBox_Console.Location = new System.Drawing.Point(986, 106);
             this.txtBox_Console.Multiline = true;
             this.txtBox_Console.Name = "txtBox_Console";
             this.txtBox_Console.ReadOnly = true;
             this.txtBox_Console.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtBox_Console.Size = new System.Drawing.Size(289, 286);
+            this.txtBox_Console.Size = new System.Drawing.Size(289, 337);
             this.txtBox_Console.TabIndex = 6;
             // 
             // btn_Pause
             // 
             this.btn_Pause.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_Pause.Location = new System.Drawing.Point(418, 418);
+            this.btn_Pause.Location = new System.Drawing.Point(418, 449);
             this.btn_Pause.Name = "btn_Pause";
-            this.btn_Pause.Size = new System.Drawing.Size(400, 50);
+            this.btn_Pause.Size = new System.Drawing.Size(462, 50);
             this.btn_Pause.TabIndex = 7;
             this.btn_Pause.Text = "Pause";
             this.btn_Pause.UseVisualStyleBackColor = true;
@@ -130,17 +108,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1225, 480);
+            this.ClientSize = new System.Drawing.Size(1287, 511);
             this.Controls.Add(this.btn_Pause);
             this.Controls.Add(this.txtBox_Console);
-            this.Controls.Add(this.lbl_HeureDyn);
-            this.Controls.Add(this.lbl_HeureFixe);
             this.Controls.Add(this.btn_Config);
             this.Controls.Add(this.txtBox_Inspection);
             this.Controls.Add(this.btn_Start);
             this.Controls.Add(this.panel1);
             this.Name = "RestaurantDisplay";
             this.Text = "Restaurant Manager 2018";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,8 +129,6 @@
         private System.Windows.Forms.Button btn_Start;
         private System.Windows.Forms.TextBox txtBox_Inspection;
         private System.Windows.Forms.Button btn_Config;
-        private System.Windows.Forms.Label lbl_HeureFixe;
-        private System.Windows.Forms.Label lbl_HeureDyn;
         private System.Windows.Forms.TextBox txtBox_Console;
         private System.Windows.Forms.Button btn_Pause;
     }
