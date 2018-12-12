14a5027a117a1fdaaf4672e198e28e5b619b5a8c
TheoRobineau <theo.robineau@viacesi.fr>
2018-12-12 15:12:14 +0100

    Classe Commande

---
+++ Code/RestaurantManager/Modèle/CommandePackage/Commande.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.Modèle.CommandePackage
+{
+    public class Commande
+    {
+        private int IdClient { get; set; }
+        private int Etat { get; set; }
+
+        private List<string> Entree = new List<string>();
+        private List<string> Plats = new List<string>();
+        private List<string> Desserts = new List<string>();
+
+        public Commande(int idClient)
+        {
+            IdClient = idClient;
+            Etat = 1;
+        }
+
+        public int IncrementEtat()
+        {
+            Etat++;
+            return Etat;
+        }
+
+        public List<string> AddEntree(List<string> recette)
+        {
+            foreach (string el in recette)
+            {
+                Console.WriteLine("Le Client" + IdClient + " a commande un ou une" + el);
+                Entree.Add(el);
+            }
+            return Entree;
+        }
+    }
+}
--- Code/RestaurantManager/Modèle/Lieux/Table.cs
+++ Code/RestaurantManager/Modèle/Lieux/Table.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestaurantManager.Modèle.CommandePackage;
 
 namespace RestaurantManager.Modèle.Lieux
 {
@@ -11,6 +12,7 @@ namespace RestaurantManager.Modèle.Lieux
         public int PosX { get; set; }
         public int PosY { get; set; }
         public int Size { get; set; }
+        public int IdTable { get; set; }
 
         public Table(int poxX, int posY, int size)
         {
@@ -24,5 +26,17 @@ namespace RestaurantManager.Modèle.Lieux
             string nL = Environment.NewLine;
             return "Position X : " + PosX + nL + "Position Y : " + PosY + nL + "Taille : " + Size;
         }
+
+        public Commande CreateCommande(int idClient)
+        {
+            Commande commande = new Commande(idClient);
+            for (int i = 0; i <= NbrPersonne; i++)
+            {
+                commande.AddEntree(SelectChoix("Entree", NbrPersonne));
+            }
+
+            return commande;
+        }
+
     }
 }
